bfa42b536dafbf90d141c9b89d55295ce4896d3c
pedrosilva <pedrogdsilva410@gmail.com>
2020-04-23 14:34:46 +0100

    "added netlify library"

---
+++ build/precache-manifest.91271c6c8fa7270d9c7b181799de26cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae9f97918dc9c70fcec9a1becc248c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "780e54dbf070d119a5e1",
+    "url": "/static/css/main.9420a4f8.chunk.css"
+  },
+  {
+    "revision": "96a6edc2b12f0ae67b28",
+    "url": "/static/js/2.d1f8a8b6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d1f8a8b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780e54dbf070d119a5e1",
+    "url": "/static/js/main.966fd808.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "c12c726a2480b693b20944469551ec34",
+    "url": "/static/media/bookSwapLogo.c12c726a.svg"
+  },
+  {
+    "revision": "00d428ae46347e159408896b8a429056",
+    "url": "/static/media/search.00d428ae.svg"
+  }
+]);
