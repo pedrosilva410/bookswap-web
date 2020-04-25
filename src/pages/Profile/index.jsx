aa309ee7f547d7d09d3be5317728d79c91a60bc6
pedrosilva <pedrogdsilva410@gmail.com>
2020-04-25 15:19:42 +0100

    created static User Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import UserPhoto from '../../assets/SVG/account.svg'
+import './styles.css'
 
 export default function Profile() {
     return(
-        <main></main>
+        <main>
+            <div className='userInfo'>
+                <img className='userImg' alt= 'userPhoto' src={UserPhoto}/>
+                <div className='userBox'>
+                    <div className='userText'>
+                        <h3 className='userName'>User Name</h3>
+                        <p className='userBio'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolor, quos tempora soluta sequi quae perferendis suscipit beatae officia tenetur amet, nobis maxime. Sapiente maxime voluptatum aperiam. Recusandae, in ipsam.</p>
+                    </div>
+                    <div className='userContact'>
+                        <p className='contactText'>Email: user@mail.com</p>
+                        <p className='contactText'>Phone: 911111111</p>
+                    </div>
+                </div>
+            </div>
+
+            <h3 className='booksTitle'>Books</h3>
+
+            <div className='userBooks'>
+                <div className='Books'></div>
+                <div className='Books'></div>
+                <div className='Books'></div>
+                <div className='Books'></div>
+            </div>
+        </main>
     )
 }
