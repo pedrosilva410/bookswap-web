89b54d63d693aea8430a0f913a7e74d5b53ca954
pedrosilva <pedrogdsilva410@gmail.com>
2020-05-15 15:54:49 +0100

    feature: log in works with backend, fix: signup

--- src/js/authentication-api.js
+++ src/js/authentication-api.js
@@ -1,26 +1,14 @@
 var AuthenticationApi = (() => {
-    let isEmpty = (value) => value === ""  || value === undefined || value === null
 
-    let isValid = (user, password) => {
-        return user === "user1" && password === "pwd123"
-    };
+    let login = (userName, password) => {
 
-    let login = (user, password, callback) => {
-        if (isEmpty(user)) {
-            callback({ message: "username-empty" }, undefined);
-            return;
-        }
-
-        if (isEmpty(password)) {
-            callback({ message: "password-empty" }, undefined);
-            return;
-        }
-
-        if(isValid(user, password)) {
-            callback(undefined, { token: "mock-token" }) 
-        } else {
-            callback({ message: "autententication-failed" }, undefined) 
-        }
+        return fetch("http://localhost:8080/authentication", {
+            method: 'POST',
+             headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({ "userName": userName, "password": password })
+        }).then(res => res.json())
     }
 
     return {
--- src/pages/BookDetails/index.jsx
+++ src/pages/BookDetails/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import "./styles.css";
 
 export default function BookDetails() {
-    return(
-        <main></main>
-    )
+  return (
+    <main>
+      <div className="bookDetails__bookInfo">
+        <div className="bookDetails__images">
+          <div className="bookDetails__bookImage">
+            <p className="textii">Book Image</p>
+          </div>
+          <div className="bookDetails__bookImage">
+            <p className="textii">Book Image</p>
+          </div>
+          <div className="bookDetails__bookImage">
+            <p className="textii">Book Image</p>
+          </div>
+        </div>
+        <div className="bookDetails__text">
+          <h3 className="bookDetails__bookTitle">Book Title</h3>
+          <p className="bookDetails__description">Book Description</p>
+        </div>
+      </div>
+      <button className="bookDetails__contact">Contact</button>
+    </main>
+  );
 }
